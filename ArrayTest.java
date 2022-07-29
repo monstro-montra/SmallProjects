bb4e1352057d2c2bc0c81bd1e1cd1f0b8941de7a
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-07-29 01:07:03 -0400

    Add files via upload

---
+++ ArrayTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+    class ArrayTest {
+
+        public static void main(String[] args){
+
+            Scanner in = new Scanner(System.in); //creates user input called in
+
+            int[] array; //creates an integer array called 'array'
+            array = new int[5]; //the array will store 5 values.
+
+            System.out.println("Input 5 numbers:"); //ask the user to input 5 numbers.
+
+            array[0] = in.nextInt(); //assign first number to array[0]
+            array[1] = in.nextInt(); //assign second number to array[1]
+            array[2] = in.nextInt(); //assign third number to array[2]
+            array[3] = in.nextInt(); //assign fourth number to array[3]
+            array[4] = in.nextInt(); //assign fifth number to array[4]
+
+            System.out.println("Your numbers are: "); 
+            for (int element: array){ //prints out each 'element' of the array using a for loop.
+                System.out.println(element);
+            }
+
+        }
+    }
---
+++ PrimeNumber.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+    class PrimeNumber {
+
+
+        public static void main(String[] args){
+
+
+            Scanner in = new Scanner(System.in);
+            int userNum;
+
+            System.out.println("Input your number: ");
+            while (!in.hasNextInt()){
+                System.out.println("That's not a prime number");
+                in.next();
+            }
+
+           
+            userNum = in.nextInt();
+
+            if(isPrime(userNum)){
+                System.out.println("Your number is: " + userNum);
+                System.out.println(userNum + " is a prime number!");
+            }
+            else{
+                System.out.println(userNum + " is not a prime number.");
+            }
+            
+
+
+
+        }
+        
+        //prime numbers can only be divided by itself, and 1 without remainders.
+        static boolean isPrime(int num){ 
+            if(num<=1){
+                return false; /*if number is less than or equal to 1, it is false. 
+            there are no negative prime numbers.*/}
+            for(int i=2; i<num/2; i++){  //2 is the smallest prime number.
+                
+                if((num%i == 0)) 
+                    return false; // if there is no remainder, return false.
+
+            }
+            return true; 
+
+        }
+}
+
+    
+
